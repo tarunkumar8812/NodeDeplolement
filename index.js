b4ceadc1d09878058f6b7e69b0a744759fa01482
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-10 14:23:52 +0530

    a

--- index.js
+++ index.js
@@ -12,20 +12,6 @@ app.use(cors())
 // app.use(cookieParser())
 app.use(express.json())
 
-// // error handling middleware + global middleware
-// app.use((err, req, res, next) => {
-
-//     const errorStatus = "err.status" || 500
-//     const errorMessage = "err.message" || "Something went wrong!"
-
-//     return res.status(errorStatus).json({
-//         success: false,
-//         status: errorStatus,
-//         message: errorMessage,
-//         stack: err.stack,
-//     })
-// })
-
 app.use("/", (req, res) => {
 
     res.json({ status: true, message: "successful" })
